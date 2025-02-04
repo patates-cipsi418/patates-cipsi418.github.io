2129d5c5421320cdc996101809dbea2833c7c263
Denis <denis.turk2001@outlook.com>
2025-02-04 11:28:32 -0500

    add title from route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 // Composables
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "@/views/Home.vue";
 import CV from "@/views/CV.vue";
 import Social from "@/views/Social.vue";
+import { profile } from "@/models/profile";
+import Vue from "vue";
 
-export const routes = [
+export const routes: RouteRecordRaw[] = [
   {
     path: "/",
     name: "Accueil",
@@ -16,6 +18,9 @@ export const routes = [
     path: "/CV",
     name: "CV",
     component: CV,
+    meta: {
+      title: `CV ${profile.name}`,
+    },
   },
   {
     path: "/Social",
@@ -27,6 +32,13 @@ export const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.afterEach((to, from) => {
+  // Use next tick to handle router history correctly
+  // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+  document.title =
+    (to.meta.title as string) || to.name?.toString() || profile.name;
 });
 
 export default router;
